4ae0fd98b60d67342c55bacd6f196a4b358667f6
MedoevRuslan <mr.r.m3@icloud.com>
2025-09-14 18:31:27 +0300

    feat: create blog schema & model

---
+++ src/modules/blogger-platform/blogs/domain/blog.entity.ts
@@ -0,0 +1,84 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Model, Types } from 'mongoose';
+import { CreateBlogDomainDto } from './dto/create-blog.domain.dto';
+
+@Schema({ timestamps: true })
+export class Blog {
+  _id: Types.ObjectId;
+
+  @Prop({ type: String, required: true, maxLength: 15 })
+  name: string;
+
+  @Prop({ type: String, required: true, maxLength: 500 })
+  description: string;
+
+  @Prop({
+    type: String,
+    maxLength: 100,
+    match: [
+      /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,}(\/[^\s]*)?$/,
+      'websiteUrl should be a valid HTTPS URL',
+    ],
+  })
+  websiteUrl: string;
+
+  /**
+   * Creation timestamp
+   * Explicitly defined despite timestamps: true
+   * properties without @Prop for typescript so that they are in the class instance (or in instance methods)
+   * @type {Date}
+   */
+  createdAt: Date;
+  updatedAt: Date;
+
+  deletedAt: Date | null;
+
+  @Prop({ type: Boolean, default: false })
+  isMembership: boolean;
+
+  get id() {
+    return this._id.toString();
+  }
+
+  /**
+   * Factory method to create a Blog instance
+   * @param {CreateBlogDomainDto} dto - The data transfer object for blog creation
+   * @returns {BlogDocument} The created blog document
+   * DDD started: как создать сущность, чтобы она не нарушала бизнес-правила? Делегируем это создание статическому методу
+   */
+  public static createInstance(dto: CreateBlogDomainDto): BlogDocument {
+    const blog = new this();
+    blog.name = dto.name;
+    blog.description = dto.description;
+    blog.websiteUrl = dto.websiteUrl;
+
+    blog.isMembership = false;
+
+    return blog as BlogDocument;
+  }
+
+  /**
+   * Marks the blog as deleted
+   * Throws an error if already deleted
+   * @throws {Error} If the entity is already deleted
+   * DDD сontinue: инкапсуляция (вызываем методы, которые меняют состояние\св-ва) объектов согласно правилам этого объекта
+   */
+  public markDeleted() {
+    if (this.deletedAt !== null) {
+      throw new Error('Entity already deleted');
+    }
+    this.deletedAt = new Date();
+  }
+}
+
+// create schema from class
+export const BlogSchema = SchemaFactory.createForClass(Blog);
+
+// register entity's methods in schema
+BlogSchema.loadClass(Blog);
+
+// document typing
+export type BlogDocument = HydratedDocument<Blog>;
+
+// model typing
+export type BlogModelType = Model<BlogDocument> & typeof Blog;

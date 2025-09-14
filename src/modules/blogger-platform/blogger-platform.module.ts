2819141725f8df7fa465234b5c14c56273456d64
MedoevRuslan <mr.r.m3@icloud.com>
2025-09-14 18:32:30 +0300

    feat(blogger-platform.module): setup module

--- src/modules/blogger-platform/blogger-platform.module.ts
+++ src/modules/blogger-platform/blogger-platform.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BlogsController } from './blogs/api/blogs.controller';
+import { BlogsService } from './blogs/application/blogs.service';
+import { BlogsRepository } from './blogs/infrastructure/blogs.repository';
+import { BlogsQueryRepository } from './blogs/infrastructure/query/blogs.query-repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Blog, BlogSchema } from './blogs/domain/blog.entity';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
+  controllers: [BlogsController],
+  providers: [BlogsService, BlogsRepository, BlogsQueryRepository],
 })
 export class BloggerPlatformModule {}

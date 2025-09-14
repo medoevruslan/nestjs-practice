8857db98b066b29cdf74dcbfab9efedc108721f4
MedoevRuslan <mr.r.m3@icloud.com>
2025-09-14 18:28:39 +0300

    feat(app.module): connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BloggerPlatformModule } from './modules/blogger-platform/blogger-platform.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BloggerPlatformModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-bloggers-platform'),
+    BloggerPlatformModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

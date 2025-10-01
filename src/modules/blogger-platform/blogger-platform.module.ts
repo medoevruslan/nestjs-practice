795bd9d5e6d93364c574d51c63f4d09d3bee7957
MedoevRuslan <mr.r.m3@icloud.com>
2025-10-01 14:09:20 +0300

    feat: ectend module config

--- src/modules/blogger-platform/blogger-platform.module.ts
+++ src/modules/blogger-platform/blogger-platform.module.ts
@@ -5,12 +5,27 @@ import { BlogsRepository } from './blogs/infrastructure/blogs.repository';
 import { BlogsQueryRepository } from './blogs/infrastructure/query/blogs.query-repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from './blogs/domain/blog.entity';
+import { PostsController } from './posts/api/posts.controller';
+import { PostsQueryRepository } from './posts/infrastructure/query/posts.query-repository';
+import { Post, PostSchema } from './posts/domain/post.entity';
+import { PostsRepository } from './posts/infrastructure/posts.repository';
+import { PostsService } from './posts/application/posts.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+    MongooseModule.forFeature([
+      { name: Blog.name, schema: BlogSchema },
+      { name: Post.name, schema: PostSchema },
+    ]),
   ],
-  controllers: [BlogsController],
-  providers: [BlogsService, BlogsRepository, BlogsQueryRepository],
+  controllers: [BlogsController, PostsController],
+  providers: [
+    BlogsService,
+    BlogsRepository,
+    BlogsQueryRepository,
+    PostsQueryRepository,
+    PostsRepository,
+    PostsService,
+  ],
 })
 export class BloggerPlatformModule {}

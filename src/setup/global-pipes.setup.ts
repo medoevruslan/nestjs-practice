2c147f18d33c894aa8402c87b56265f8ff564f9f
MedoevRuslan <mr.r.m3@icloud.com>
2025-10-05 20:25:18 +0300

    feat: add global pipe

--- src/setup/global-pipes.setup.ts
+++ src/setup/global-pipes.setup.ts
@@ -1,5 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ParseObjectIdOrBadRequestPipe } from '../core/pipes/ParseObjectIdOrBadRequestPipe';
 
 export function globalPipesSetup(app: INestApplication) {
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true }),
+    app.get(ParseObjectIdOrBadRequestPipe), // validate ObjectId globally
+  );
 }

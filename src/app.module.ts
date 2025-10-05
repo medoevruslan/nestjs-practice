38685a03eaf8fbb0026ddd4e3ca3c387dee992f2
MedoevRuslan <mr.r.m3@icloud.com>
2025-10-05 12:41:06 +0300

    feat: add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BloggerPlatformModule } from './modules/blogger-platform/blogger-platform.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserAccountModule } from './modules/user-account/user-account.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-bloggers-platform'),
     BloggerPlatformModule,
+    UserAccountModule,
   ],
   controllers: [AppController],
   providers: [AppService],

0c47889eb4c6277ca266d96d9b16ce87560fa34f
MedoevRuslan <mr.r.m3@icloud.com>
2026-02-14 19:21:32 +0200

    feat: add fields

--- src/modules/auth/auth.config.ts
+++ src/modules/auth/auth.config.ts
@@ -1,12 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ms } from 'ms';
 
 @Injectable()
 export class AuthConfig {
   skipPasswordCheck: boolean;
+  jwtSecret: string;
+  expiresIn: ms.StringValue;
 
   constructor(@Inject() private readonly configService: ConfigService) {
     this.skipPasswordCheck =
       this.configService.get<string>('SKIP_PASSWORD_CHECK') === 'true';
+    this.jwtSecret = this.configService.get('JWT_SECRET') ?? 'secret';
+    this.expiresIn = this.configService.get('JWT_EXPIRES_IN') ?? '15m';
   }
 }

bca8a6083ec293c22cbc94620f47ff9eea1cbd4a
MedoevRuslan <mr.r.m3@icloud.com>
2026-04-22 15:32:23 +0300

    refac: change implementation

--- src/modules/auth/auth.config.ts
+++ src/modules/auth/auth.config.ts
@@ -1,9 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ms } from 'ms';
+import ms from 'ms';
+import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthConfig {
+export class AuthConfig implements JwtOptionsFactory {
   skipPasswordCheck: boolean;
   jwtSecret: string;
   expiresIn: ms.StringValue;
@@ -13,5 +14,12 @@ export class AuthConfig {
       this.configService.get<string>('SKIP_PASSWORD_CHECK') === 'true';
     this.jwtSecret = this.configService.get('JWT_SECRET') ?? 'secret';
     this.expiresIn = this.configService.get('JWT_EXPIRES_IN') ?? '15m';
+  }
+
+  createJwtOptions(): JwtModuleOptions {
+    return {
+      secret: this.jwtSecret,
+      signOptions: { expiresIn: this.expiresIn },
+    };
   }
 }

d4b96a988404d99f5d4d355aa298f2ca1d7d8e70
MedoevRuslan <mr.r.m3@icloud.com>
2026-04-22 12:44:59 +0300

    feat: add methods stubs

--- src/modules/auth/application/auth.service.ts
+++ src/modules/auth/application/auth.service.ts
@@ -1,17 +1,20 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersRepository } from '../../user-account/infrastructure/users.repository';
 import { CryptoService } from '../../user-account/application/crypto-service';
 import { AuthConfig } from '../auth.config';
+import { UsersService } from '../../user-account/application/users.service';
+import { RegisterUserDto } from '../dto/register-user.dto';
+import { NewPasswordDto } from '../dto/new-password.dto';
+import { LoginDto } from '../dto/login.dto';
 
 @Injectable()
 export class AuthService {
   private readonly jswService: JwtService;
 
   constructor(
-    @Inject() private readonly usersRepository: UsersRepository,
     @Inject() private readonly cryptoService: CryptoService,
     @Inject() private readonly authConfig: AuthConfig,
+    @Inject() private readonly usersService: UsersService,
   ) {
     this.jswService = new JwtService({
       secret: this.authConfig.jwtSecret,
@@ -19,12 +22,12 @@ export class AuthService {
     });
   }
 
-  async login(email: string, password: string) {
-    const user = await this.usersRepository.findByEmailOrFail(email);
+  async login(dto: LoginDto) {
+    const user = await this.usersService.getByEmail(dto.email);
 
     if (!this.authConfig.skipPasswordCheck) {
       const isPass = await this.cryptoService.checkPassword(
-        password,
+        dto.password,
         user.password,
       );
 
@@ -33,11 +36,25 @@ export class AuthService {
       }
     }
 
-    const payload = { email, id: user.id };
+    const payload = { email: dto.email, id: user.id };
 
     const accessToken = this.jswService.sign(payload);
     const refreshToken = this.jswService.sign(payload, { expiresIn: '7d' });
 
     return { accessToken, refreshToken };
   }
+
+  async register(dto: RegisterUserDto) {
+    await this.usersService.createUser(dto);
+  }
+
+  async confirmRegistration(code: string) {
+    // confirm registration with code
+  }
+
+  async sendRegistrationEmail(email: string) {}
+
+  async newPassword(dto: NewPasswordDto) {}
+
+  async recoveryPassword(email: string) {}
 }

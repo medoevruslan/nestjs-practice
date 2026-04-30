6dbca35ae9aa189fd35580d1eef5ef34df05dd2f
MedoevRuslan <mr.r.m3@icloud.com>
2026-04-30 15:02:23 +0300

    feat(users.service): add methods

--- src/modules/user-account/application/users.service.ts
+++ src/modules/user-account/application/users.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { UsersRepository } from '../infrastructure/users.repository';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserModelType } from '../domain/user.entity';
+import { User, UserDocument, UserModelType } from '../domain/user.entity';
 import { CryptoService } from './crypto-service';
 
 @Injectable()
@@ -13,6 +13,10 @@ export class UsersService {
     @Inject() private cryptoService: CryptoService,
   ) {}
 
+  async save(user: UserDocument) {
+    await this.usersRepository.save(user);
+  }
+
   async getById(id: string) {
     return this.usersRepository.findByIdOrFail(id);
   }
@@ -21,8 +25,16 @@ export class UsersService {
     return this.usersRepository.findByEmailOrFail(email);
   }
 
+  async getByEmailNullable(email: string) {
+    return this.usersRepository.findByEmailOrNull(email);
+  }
+
   async getByPasswordRecoveryCode(code: string) {
     return this.usersRepository.findByPasswordRecoveryCodeOrFail(code);
+  }
+
+  async getByPasswordRecoveryCodeNullable(code: string) {
+    return this.usersRepository.findByPasswordRecoveryCodeOrNull(code);
   }
 
   async getByEmailConfirmationCode(code: string) {
@@ -47,5 +59,18 @@ export class UsersService {
     user.markDeleted();
     await this.usersRepository.save(user);
     return user.id;
+  }
+
+  async createPasswordRecoveryCode(email: string): Promise<string | null> {
+    const found = await this.usersRepository.findByEmailOrNull(email);
+    if (found) {
+      const code = crypto.randomUUID();
+      found.passwordRecoveryCode = code;
+      found.confirmationCodeExpiration = new Date(Date.now() + 1000 * 60 * 5);
+      await this.usersRepository.save(found);
+      return code;
+    }
+
+    return null;
   }
 }

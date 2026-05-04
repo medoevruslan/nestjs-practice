1135df4c071ef30f18b22d2e6a469ee1aba4062a
MedoevRuslan <mr.r.m3@icloud.com>
2026-05-04 12:44:42 +0300

    feat: use trim decorator

--- src/modules/auth/api/input-dto/login.input-dto.ts
+++ src/modules/auth/api/input-dto/login.input-dto.ts
@@ -1,7 +1,9 @@
 import { IsString, Length, MinLength } from 'class-validator';
+import { Trim } from '../../../../core/decorators/transform';
 
 export class LoginInputDto {
   @IsString()
+  @Trim()
   @MinLength(3)
   loginOrEmail: string;
 
--- src/modules/auth/api/input-dto/register-user.input-dto.ts
+++ src/modules/auth/api/input-dto/register-user.input-dto.ts
@@ -1,8 +1,10 @@
 import { IsEmail, Length, Matches } from 'class-validator';
+import { Trim } from '../../../../core/decorators/transform';
 
 export class RegisterUserInputDto {
-  @Length(3, 10)
   @Matches(/^[a-zA-Z0-9_-]*$/)
+  @Trim()
+  @Length(3, 10)
   login: string;
 
   @IsEmail()

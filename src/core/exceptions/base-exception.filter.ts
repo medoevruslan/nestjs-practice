61bbd11ae700cc9cc5f1bbce541bc303910b5173
MedoevRuslan <mr.r.m3@icloud.com>
2026-05-04 14:41:05 +0300

    feat: add common exception filter

---
+++ src/core/exceptions/base-exception.filter.ts
@@ -0,0 +1,54 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { ErrorResponseBody } from './domain-exceptions';
+import { Request, Response } from 'express';
+import { DomainExceptionCode } from './domain-exception-codes';
+
+@Catch()
+export class BaseExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const message = exception.message;
+    const status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+
+  private buildResponseBody(
+    requestUrl: string,
+    message: string,
+  ): ErrorResponseBody {
+    //TODO: Replace with getter from configService. will be in the following lessons
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    if (isProduction) {
+      return {
+        timestamp: new Date().toISOString(),
+        path: null,
+        message: 'Some error occurred',
+        extensions: [],
+        code: DomainExceptionCode.InternalServerError,
+      };
+    }
+
+    return {
+      timestamp: new Date().toISOString(),
+      path: requestUrl,
+      message,
+      extensions: [],
+      code: DomainExceptionCode.InternalServerError,
+    };
+  }
+}

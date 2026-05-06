f547049c2b9d3e35bac86f8311657ae536eb3fbd
MedoevRuslan <mr.r.m3@icloud.com>
2026-05-06 16:31:51 +0300

    refac: clean up code

--- src/modules/user-account/application/users.service.ts
+++ src/modules/user-account/application/users.service.ts
@@ -1,14 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UsersRepository } from '../infrastructure/users.repository';
-import { CreateUserDto } from '../dto/create-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument, UserModelType } from '../domain/user.entity';
 import { CryptoService } from './crypto-service';
-import {
-  DomainException,
-  Extensions,
-} from '../../../core/exceptions/domain-exceptions';
-import { DomainExceptionCode } from '../../../core/exceptions/domain-exception-codes';
 
 @Injectable()
 export class UsersService {
@@ -52,46 +46,6 @@ export class UsersService {
 
   async getByEmailConfirmationCodeNullable(code: string) {
     return this.usersRepository.findByEmailConfirmationCodeOrNull(code);
-  }
-
-  async createUser(dto: CreateUserDto): Promise<string> {
-    await this.ensureLoginAndEmailAreUnique(dto.login, dto.email);
-
-    const hashedPassword = await this.cryptoService.hashPassword(dto.password);
-
-    const user = this.UserModel.createInstance({
-      login: dto.login,
-      email: dto.email,
-      password: hashedPassword,
-    });
-
-    await this.usersRepository.save(user);
-    return user.id;
-  }
-
-  private async ensureLoginAndEmailAreUnique(login: string, email: string) {
-    const [userWithLogin, userWithEmail] = await Promise.all([
-      this.usersRepository.findByLoginOrNull(login),
-      this.usersRepository.findByEmailOrNull(email),
-    ]);
-
-    const extensions: Extensions[] = [];
-
-    if (userWithLogin) {
-      extensions.push({ field: 'login', message: 'Login already exists' });
-    }
-
-    if (userWithEmail) {
-      extensions.push({ field: 'email', message: 'Email already exists' });
-    }
-
-    if (extensions.length) {
-      throw new DomainException({
-        code: DomainExceptionCode.BadRequest,
-        message: 'User already exists',
-        extensions,
-      });
-    }
   }
 
   async deleteUser(id: string): Promise<string> {

983e9953279d26b8b9681d12fb764831d4037f58
MedoevRuslan <mr.r.m3@icloud.com>
2026-05-22 15:09:47 +0300

    feat(exception-filter): add logger & config service

--- src/core/exceptions/base-exception.filter.ts
+++ src/core/exceptions/base-exception.filter.ts
@@ -3,13 +3,19 @@ import {
   Catch,
   ExceptionFilter,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { ErrorResponseBody } from './domain-exceptions';
 import { Request, Response } from 'express';
 import { DomainExceptionCode } from './domain-exception-codes';
+import { ConfigService } from '@nestjs/config';
 
 @Catch()
 export class AllHttpExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllHttpExceptionsFilter.name);
+
+  constructor(private readonly configService: ConfigService) {}
+
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -17,6 +23,8 @@ export class AllHttpExceptionsFilter implements ExceptionFilter {
 
     const message = exception.message;
     const status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    this.logger.error(message, exception.stack);
 
     response.status(status).json({
       statusCode: status,
@@ -30,8 +38,7 @@ export class AllHttpExceptionsFilter implements ExceptionFilter {
     requestUrl: string,
     message: string,
   ): ErrorResponseBody {
-    //TODO: Replace with getter from configService. will be in the following lessons
-    const isProduction = process.env.NODE_ENV === 'production';
+    const isProduction = this.configService.get('NODE_ENV') === 'production';
 
     if (isProduction) {
       return {

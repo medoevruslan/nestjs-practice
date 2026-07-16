3ac75c9b503e213b8bc5ac2029dd97394922ed7f
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-16 13:11:17 +0300

    refac(posts.service): clean up code

--- src/modules/blogger-platform/posts/application/posts.service.ts
+++ src/modules/blogger-platform/posts/application/posts.service.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { UpdatePostDto } from '../dto/update-post.dto';
 import { PostsRepository } from '../infrastructure/posts.repository';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post, PostModelType } from '../domain/post.entity';
@@ -10,18 +9,4 @@ export class PostsService {
     @InjectModel(Post.name) private PostModel: PostModelType,
     @Inject() private postsRepository: PostsRepository,
   ) {}
-
-  async updatePost(id: string, dto: UpdatePostDto) {
-    const post = await this.postsRepository.getByIdOrFail(id);
-    post.update(dto);
-    await this.postsRepository.save(post);
-    return post.id;
-  }
-
-  async deletePost(id: string) {
-    const post = await this.postsRepository.getByIdOrFail(id);
-    post.markDeleted();
-    await this.postsRepository.save(post);
-    return post.id;
-  }
 }

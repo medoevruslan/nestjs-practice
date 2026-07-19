ebd56cdf3a70480494363062012891e2c9b2b38a
ruslan m <ruslanm@siema.com.ua>
2026-07-19 17:54:41 +0300

    refac: simplify logic

--- src/modules/auth/strategies/basic.strategy.ts
+++ src/modules/auth/strategies/basic.strategy.ts
@@ -13,23 +13,13 @@ export class BasicStrategy extends PassportStrategy(Strategy) {
         super({ passReqToCallback: true });
     }
 
-    validate(req: Request) {
-        const auth = req.headers.authorization
-
-        if (!auth) this.throwUnauthorized()
-
-        const [authType, data] = auth.split(' ');
-
-        if (authType !== 'Basic') this.throwUnauthorized();
-
+    validate(req: Request, username: string, password: string) {
         const name = this.configService.get<string>('ADMIN_NAME')
         const passw = this.configService.get<string>('ADMIN_PASSWORD')
 
-        const creds = Buffer.from(data, 'base64').toString('utf-8')
+        if (name === username && passw === password) return true
+        this.throwUnauthorized()
 
-        if (creds !== `${name}:${passw}`) this.throwUnauthorized()
-
-        return true
     }
 
     private throwUnauthorized(): never {

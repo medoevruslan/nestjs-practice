fc6cdddd2ce54679071b57c81707906c6354bb71
ruslan m <ruslanm@siema.com.ua>
2026-07-19 18:22:53 +0300

    feat: add decorator

---
+++ src/core/decorators/auth/create-param.decorator.ts
@@ -0,0 +1,17 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { Request } from "express";
+
+export const CurrentUserId = createParamDecorator(
+    (data: unknown, context: ExecutionContext) => {
+        const request = context.switchToHttp().getRequest<Request>()
+
+        const user = request.user
+
+        if (!user) {
+            console.error('No user found. This decorator should be used with AuthGuard')
+            return 'unknown_id'
+        }
+
+        return user.id
+    },
+);

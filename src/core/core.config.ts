dbb582e33653b7544a4b92555a0bdd048114abb2
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-23 12:43:25 +0300

    fix(core.config): parse errors, show user

--- src/core/core.config.ts
+++ src/core/core.config.ts
@@ -1,28 +1,41 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { IsStringWithTrim } from "./decorators/validation/is-string-with-trim";
-import { IsEnum, IsNumber } from "class-validator";
-
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IsStringWithTrim } from './decorators/validation/is-string-with-trim';
+import { IsEnum, IsNumber, validateSync } from 'class-validator';
 
 enum Environment {
-    PRODUCTION = 'production',
-    DEVELOPMENT = 'develpopment',
-    STAGING = 'staging',
-    TESTING = 'testing'
+  PRODUCTION = 'production',
+  DEVELOPMENT = 'development',
+  STAGING = 'staging',
+  TESTING = 'testing',
 }
 
 @Injectable()
 export class CoreConfig {
-    @IsNumber()
-    port: number = Number(this.configService.get('PORT'));
+  @IsNumber(undefined, { message: 'Please define port, ex.: 3000' })
+  port: number = Number(this.configService.get('PORT'));
 
-    @IsStringWithTrim(1)
-    mongoUri: string = String(this.configService.get('MONGODB_URI'))
+  @IsStringWithTrim(1)
+  mongoUri: string = String(this.configService.get('MONGODB_URI'));
 
-    @IsEnum(Environment)
-    env: string = String(this.configService.get('NODE_ENV'))
+  @IsEnum(Environment)
+  env: string = String(this.configService.get('NODE_ENV'));
 
-    constructor(private readonly configService: ConfigService) {
+  constructor(private readonly configService: ConfigService) {
+    const valid = validateSync(this);
+    if (valid.length) {
+      const errors = valid.reduce<Record<string, string[]>>((err, val, idx) => {
+        if (val.constraints) {
+          err[val.property] = Object.entries(val.constraints).map(
+            ([key, message]) => `${idx}) ${key}:: ${message}`,
+          );
+        }
 
+        return err;
+      }, {});
+      throw new Error(
+        `Invalid environment configuration:\n${JSON.stringify(errors, null, 2)}`,
+      );
     }
+  }
 }

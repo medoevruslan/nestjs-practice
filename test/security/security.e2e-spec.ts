354495eaccdc6ea58796410d979f1ab2ba0c8d09
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-26 20:00:17 +0300

    refac: set up test

--- test/security/security.e2e-spec.ts
+++ test/security/security.e2e-spec.ts
@@ -1,10 +1,11 @@
 import request from 'supertest';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../../src/app.module';
 import { appSetup } from '../../src/setup/app.setup';
 import { Connection } from 'mongoose';
 import { getConnectionToken } from '@nestjs/mongoose';
+import { createTestUser, loginTestUser } from '../create-test-user';
 
 describe('Security (e2e)', () => {
   let app: INestApplication;
@@ -22,6 +23,16 @@ describe('Security (e2e)', () => {
     if (!connection.db) {
       throw Error('Testing db is not available');
     }
+
+    const resetAllRes = await request(app.getHttpServer()).delete(
+      '/api/testing/all-data',
+    );
+
+    expect(resetAllRes.status).toBe(HttpStatus.NO_CONTENT);
+
+    const createdRes = await createTestUser(app.getHttpServer());
+
+    expect(createdRes.status).toBe(HttpStatus.CREATED);
   });
 
   afterAll(async () => {
@@ -29,7 +40,13 @@ describe('Security (e2e)', () => {
   });
 
   it('should return device sessions empty list', async () => {
-    const res = await request(app.getHttpServer()).get('/api/security/devices');
+    const loginRes = await loginTestUser(app.getHttpServer());
+    expect(loginRes.status).toBe(HttpStatus.OK);
+    expect(loginRes.body.accessToken).toBeDefined();
+
+    const res = await request(app.getHttpServer())
+      .get('/api/security/devices')
+      .auth(loginRes.body.accessToken, { type: 'bearer' });
 
     expect(res.body.length).toBe(0);
   });

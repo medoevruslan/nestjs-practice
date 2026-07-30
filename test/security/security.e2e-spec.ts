41a6fb5865afcdef32a6006b3d429b80af38ee09
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-30 19:50:23 +0300

    refac(tests): update tests

--- test/security/security.e2e-spec.ts
+++ test/security/security.e2e-spec.ts
@@ -9,6 +9,7 @@ import { createTestUser, loginTestUser } from '../create-test-user';
 
 describe('Security (e2e)', () => {
   let app: INestApplication;
+  let accessToken: string;
 
   beforeAll(async () => {
     const builder = Test.createTestingModule({ imports: [AppModule] });
@@ -35,23 +36,79 @@ describe('Security (e2e)', () => {
     expect(createdRes.status).toBe(HttpStatus.CREATED);
   });
 
+  beforeEach(async () => {
+    const loginRes = await loginTestUser(app.getHttpServer());
+    expect(loginRes.status).toBe(HttpStatus.OK);
+    expect(loginRes.body.accessToken).toBeDefined();
+
+    accessToken = loginRes.body.accessToken;
+  });
+
+  afterEach(async () => {
+    await cleanUpSessions(app, accessToken);
+  });
+
   afterAll(async () => {
     await app?.close();
   });
 
   it('should create device session on login', async () => {
-    const loginRes = await loginTestUser(app.getHttpServer());
-    expect(loginRes.status).toBe(HttpStatus.OK);
-    expect(loginRes.body.accessToken).toBeDefined();
-
     const deviceAuthRes = await request(app.getHttpServer())
       .get('/api/security/devices')
-      .auth(loginRes.body.accessToken, { type: 'bearer' })
+      .auth(accessToken, { type: 'bearer' })
       .expect(HttpStatus.OK);
 
     expect(deviceAuthRes.body.length).toBe(1);
   });
 
-  it('should delete all sessions on request', async () => { });
-  it('should delete session by id on request', async () => { });
+  it('should delete all sessions on request', async () => {
+    const deviceAuthRes1 = await request(app.getHttpServer())
+      .get('/api/security/devices')
+      .auth(accessToken, { type: 'bearer' })
+      .expect(HttpStatus.OK);
+
+    expect(deviceAuthRes1.body.length).toBe(1);
+
+    await request(app.getHttpServer())
+      .delete('/api/security/devices')
+      .auth(accessToken, { type: 'bearer' })
+      .expect(HttpStatus.NO_CONTENT);
+
+    const deviceAuthRes2 = await request(app.getHttpServer())
+      .get('/api/security/devices')
+      .auth(accessToken, { type: 'bearer' })
+      .expect(HttpStatus.OK);
+
+    expect(deviceAuthRes2.body.length).toBe(0);
+  });
+
+  it('should delete session by id on request', async () => {
+    const deviceAuthRes1 = await request(app.getHttpServer())
+      .get('/api/security/devices')
+      .auth(accessToken, { type: 'bearer' })
+      .expect(HttpStatus.OK);
+
+    expect(deviceAuthRes1.body.length).toBe(1);
+
+    const deviceId = deviceAuthRes1.body[0].deviceId;
+
+    await request(app.getHttpServer())
+      .delete(`/api/security/devices/${deviceId}`)
+      .auth(accessToken, { type: 'bearer' })
+      .expect(HttpStatus.NO_CONTENT);
+
+    const deviceAuthRes2 = await request(app.getHttpServer())
+      .get(`/api/security/devices`)
+      .auth(accessToken, { type: 'bearer' })
+      .expect(HttpStatus.OK);
+
+    expect(deviceAuthRes2.body.length).toBe(0);
+  });
 });
+
+const cleanUpSessions = async (app: INestApplication, token: string) => {
+  await request(app.getHttpServer())
+    .delete('/api/security/devices')
+    .auth(token, { type: 'bearer' })
+    .expect(HttpStatus.NO_CONTENT);
+};

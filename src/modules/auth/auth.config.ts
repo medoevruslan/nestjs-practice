1e5130e1319fb333086ebcb1b173ec3c29c0f193
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-30 19:47:16 +0300

    refac(auth.config): add getters

--- src/modules/auth/auth.config.ts
+++ src/modules/auth/auth.config.ts
@@ -9,21 +9,38 @@ import { IsMsDuration } from 'src/core/decorators/validation/is-ms-duration';
 @Injectable()
 export class AuthConfig implements JwtOptionsFactory {
   @IsBoolean()
-  skipPasswordCheck: boolean = this.configService.get<string>('SKIP_PASSWORD_CHECK') === 'true';
+  skipPasswordCheck: boolean =
+    this.configService.get<string>('SKIP_PASSWORD_CHECK') === 'true';
 
   @IsStringWithTrim(4)
   jwtSecret: string = String(this.configService.get('JWT_SECRET'));
 
   @IsMsDuration()
-  expiresIn: ms.StringValue = this.configService.get<ms.StringValue>('JWT_EXPIRES_IN') as ms.StringValue;
+  expiresIn: ms.StringValue = this.configService.get<ms.StringValue>(
+    'JWT_EXPIRES_IN',
+  ) as ms.StringValue;
 
   @IsStringWithTrim(1)
-  adminName: string = String(this.configService.get<string>('ADMIN_NAME'))
+  adminName: string = String(this.configService.get<string>('ADMIN_NAME'));
 
   @IsStringWithTrim(4)
-  adminPassword: string = String(this.configService.get('ADMIN_PASSWORD'))
+  adminPassword: string = String(this.configService.get('ADMIN_PASSWORD'));
 
-  constructor(private readonly configService: ConfigService) { }
+  constructor(private readonly configService: ConfigService) {}
+
+  getJwtConfig() {
+    return {
+      secret: this.jwtSecret,
+      expiresIn: this.expiresIn,
+    };
+  }
+
+  getAdminCredentials() {
+    return {
+      name: this.adminName,
+      password: this.adminPassword,
+    };
+  }
 
   createJwtOptions(): JwtModuleOptions {
     return {

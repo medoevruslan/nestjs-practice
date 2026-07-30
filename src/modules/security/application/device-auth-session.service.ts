f960cbdd8606136e46a1f23bb94c5167ad7f291e
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-30 19:49:00 +0300

    feat: add method

--- src/modules/security/application/device-auth-session.service.ts
+++ src/modules/security/application/device-auth-session.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DeviceAuthSessionRepository } from '../infrastructure/device-auth-session.repository';
 
 @Injectable()
 export class DeviceAuthSessionService {
-  constructor() {}
+  constructor(
+    @Inject()
+    private readonly deviceAuthSessionRepository: DeviceAuthSessionRepository,
+  ) {}
 
   async getAll() {}
 
   async getByUserId() {}
+
+  async getDeviceAuthSessionByDeviceIdOrFault(id: string) {
+    return this.deviceAuthSessionRepository.getDeviceAuthSessionByDeviceIdOrFault(
+      id,
+    );
+  }
 }

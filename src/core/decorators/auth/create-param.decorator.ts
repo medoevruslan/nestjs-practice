fd94c2f056cf961c89b5f89635a4ac835f1842f0
MedoevRuslan <mr.r.m3@icloud.com>
2026-07-30 19:45:25 +0300

    refac: allign code

--- src/core/decorators/auth/create-param.decorator.ts
+++ src/core/decorators/auth/create-param.decorator.ts
@@ -1,17 +1,27 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { Request } from "express";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
 export const CurrentUserId = createParamDecorator(
-    (data: unknown, context: ExecutionContext) => {
-        const request = context.switchToHttp().getRequest<Request>()
+  (data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest<Request>();
 
-        const user = request.user
+    const user = request.user;
 
-        if (!user) {
-            console.error('No user found. This decorator should be used with AuthGuard')
-            return 'unknown_id'
-        }
+    if (!user) {
+      console.error(
+        'No user found. This decorator should be used with AuthGuard',
+      );
+      return 'unknown_id';
+    }
 
-        return user.id
-    },
+    return user.id;
+  },
+);
+
+export const Cookies = createParamDecorator(
+  (data: string | undefined, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest<Request>();
+
+    return data ? req.cookies?.[data] : req.cookies;
+  },
 );

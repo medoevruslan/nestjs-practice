973e36c9bf57122037aa9af89b5776e18d7c28b6
MedoevRuslan <mr.r.m3@icloud.com>
2026-08-02 18:55:42 +0300

    feat: add method

--- src/modules/security/application/device-auth-session.service.ts
+++ src/modules/security/application/device-auth-session.service.ts
@@ -1,5 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DeviceAuthSessionRepository } from '../infrastructure/device-auth-session.repository';
+import bcrypt from 'bcrypt';
+import { DomainException } from '../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../core/exceptions/domain-exception-codes';
 
 @Injectable()
 export class DeviceAuthSessionService {
@@ -22,5 +25,26 @@ export class DeviceAuthSessionService {
     const session = await this.getDeviceAuthSessionByDeviceIdOrFault(id);
     session.markDeleted();
     await this.deviceAuthSessionRepository.save(session);
+  }
+
+  async validateRefreshTokenForSession(deviceId: string, refreshToken: string) {
+    const found =
+      await this.deviceAuthSessionRepository.getDeviceAuthSessionByDeviceIdOrFault(
+        deviceId,
+      );
+
+    const isTokenValid = await bcrypt.compare(
+      refreshToken,
+      found.refreshTokenHash!,
+    );
+
+    if (!isTokenValid) {
+      await this.revokeDeviceAuthSessionByDeviceId(deviceId);
+
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Invalid authorization data',
+      });
+    }
   }
 }

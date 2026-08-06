dff7137710bba632a3617bfd1b4e25f006065ada
ruslan m <ruslanm@siema.com.ua>
2026-08-06 15:12:57 +0300

    feat: update tests

--- test/security/security.e2e-spec.ts
+++ test/security/security.e2e-spec.ts
@@ -25,35 +25,23 @@ describe('Security (e2e)', () => {
     if (!connection.db) {
       throw Error('Testing db is not available');
     }
-
-    const resetAllRes = await request(app.getHttpServer()).delete(
-      '/api/testing/all-data',
-    );
-
-    expect(resetAllRes.status).toBe(HttpStatus.NO_CONTENT);
-
-    const createdRes = await createTestUser(app.getHttpServer());
-
-    expect(createdRes.status).toBe(HttpStatus.CREATED);
   });
 
   beforeEach(async () => {
-    beforeEach(async () => {
-      const resetRes = await request(app.getHttpServer()).delete(
-        '/api/testing/all-data',
-      );
+    const resetRes = await request(app.getHttpServer()).delete(
+      '/api/testing/all-data',
+    );
 
-      expect(resetRes.status).toBe(HttpStatus.NO_CONTENT);
+    expect(resetRes.status).toBe(HttpStatus.NO_CONTENT);
 
-      const createdRes = await createTestUser(app.getHttpServer());
-      expect(createdRes.status).toBe(HttpStatus.CREATED);
+    const createdRes = await createTestUser(app.getHttpServer());
+    expect(createdRes.status).toBe(HttpStatus.CREATED);
 
-      const loginRes = await loginTestUser(app.getHttpServer());
-      expect(loginRes.status).toBe(HttpStatus.OK);
+    const loginRes = await loginTestUser(app.getHttpServer());
+    expect(loginRes.status).toBe(HttpStatus.OK);
 
-      accessToken = loginRes.body.accessToken;
-      refreshToken = loginRes.headers['set-cookie'][0];
-    });
+    accessToken = loginRes.body.accessToken;
+    refreshToken = loginRes.headers['set-cookie'][0];
   });
 
   afterAll(async () => {
@@ -69,7 +57,7 @@ describe('Security (e2e)', () => {
     expect(deviceAuthRes.body.length).toBe(1);
   });
 
-  it('should delete all sessions on request', async () => {
+  it('should delete all sessions on request except current', async () => {
     const deviceAuthRes1 = await request(app.getHttpServer())
       .get('/api/security/devices')
       .auth(accessToken, { type: 'bearer' })
@@ -80,6 +68,7 @@ describe('Security (e2e)', () => {
     await request(app.getHttpServer())
       .delete('/api/security/devices')
       .auth(accessToken, { type: 'bearer' })
+      .set('Cookie', refreshToken)
       .expect(HttpStatus.NO_CONTENT);
 
     const deviceAuthRes2 = await request(app.getHttpServer())
@@ -87,7 +76,7 @@ describe('Security (e2e)', () => {
       .auth(accessToken, { type: 'bearer' })
       .expect(HttpStatus.OK);
 
-    expect(deviceAuthRes2.body.length).toBe(0);
+    expect(deviceAuthRes2.body.length).toBe(1);
   });
 
   it('should delete session by id on request', async () => {

caa8997b895049c5d98e2eaafc404e12727f6c37
ruslan m <ruslanm@siema.com.ua>
2026-08-06 15:11:36 +0300

    feat(app.setup): add setup

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -2,12 +2,14 @@ import { INestApplication } from '@nestjs/common';
 import { globalPrefixSetup } from './global-prefix.setup';
 import { swaggerSetup } from './swagger.setup';
 import { globalPipesSetup } from './global-pipes.setup';
+import cookieParser from 'cookie-parser';
 
 export function appSetup(app: INestApplication) {
   globalPipesSetup(app);
   globalPrefixSetup(app);
   swaggerSetup(app);
   app.enableCors();
+  app.use(cookieParser());
 
   const express = app.getHttpAdapter().getInstance();
   express.set('trust proxy', true);
